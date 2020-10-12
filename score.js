1fbcde3f4dbdc9ba9fc9e00eb2174feb1d03e517
Jeremiah Holden <jeremiah@cardconnectcp.com>
2020-10-12 07:17:38 -0700

    stored highscore in local storage

--- score.js
+++ score.js
@@ -1,1 +1,14 @@
-document.getElementById("result").innerHTML = localStorage.getItem("username");
+var username = 0;
+var gameScore = 0;
+var highscore = localStorage.getItem("highscore");
+
+if(highscore !== null){
+    if (gameScore > highscore) {
+        localStorage.setItem("highscore", gameScore);      
+    }
+}
+else{
+    localStorage.setItem("highscore", highscore);
+}
+
+// document.getElementById("result").innerHTML = localStorage.getItem("username");
